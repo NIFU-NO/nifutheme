baec249252e2a2c16c8d01b5da8aa2646f808c94
Henrik Karlstrøm <35686798+henrikkarlstrom@users.noreply.github.com>
2023-04-25 13:59:46 +0200

    First working commit

--- R/nifu_pal.R
+++ R/nifu_pal.R
@@ -1,59 +1,141 @@
-#' NIFU-palett for ggplot2-grafer
+#' Specify NIFU colours
 #'
-#' @param palette Type palett
-#' @param alpha Transparens, en verdi mellom 0 og 1
+#' @param type Type of palette, defaulting to a discrete colour palette
+#' @param ... Additional arguments passed to ggplot2
 #'
-#' @export nifu_pal
-#' @importFrom grDevices rgb
+
+nifu_cols <- function(type = "main", ...){
+  
+  if(!type %in% c("main", "reds", "blues")){
+    stop(paste0("The palette '", type, 
+               "' is not available for this function. Please choose one of 
+               c('main', 'reds', 'blues') as the type argument."))
+  }
+  
+  if(type == "main"){
+    cols_used <- c(
+      "#C84957",
+      "#A6A6A6",
+      "#2D8E9F",
+      "#EDE2D2",
+      "#DBD2E0",
+      "#E8AE59")
+  } else if (type == "reds"){
+    cols_used <- c(
+      "#E8B0B7",
+      "#DE919A",
+      "#D5727D",
+      "#C84957",
+      "#BC3848",
+      "#9D2F3C",
+      "#7E2630")
+  } else if (type == "blues"){
+    cols_used <- c(
+      "#90D4E0",
+      "#70C7D7",
+      "#50BBCE",
+      "#36AABF",
+      "#2D8E9F",
+      "#24727F",
+      "#1B555F")
+  }
+  
+  cols <- c(...)
+  
+  if(is.null(cols))
+    return (cols_used)
+  
+  cols_used[cols]
+}
+
+#' Create a color palette in NIFU colours
 #'
-#' @examples
-#' library(scales)
-#' show_col(nifu_pal("default")(6))
-nifu_pal <- function(palette = c("default"), alpha = 1){
-  palette <- match.arg(palette)
+#' @param palette Type of palette
+#' @param reverse Specify direction of colour palette
+#' @param ... Additional arguments to colorRampPalette
+#' 
+#' @importFrom grDevices colorRampPalette
+#'
+nifu_pal <- function(palette = "main",
+                     reverse = FALSE,
+                     ...){
   
-  if(alpha > 1L | alpha <= 0L) stop("alpha must be in range (0, 1]")
+  pal <- nifu_cols(palette)
   
-  raw_cols <- c("nifu_red"    = rgb(200/255, 73/255, 87/255,
-                                    alpha = alpha),
-                "nifu_dark"   = rgb(64/255, 64/255, 64/255,
-                                    alpha = alpha),
-                "nifu_tan"    = rgb(237/255, 226/255, 210/255,
-                                    alpha = alpha),
-                "nifu_teal"   = rgb(45/255, 142/255, 159/255,
-                                    alpha = alpha),
-                "nifu_pink"   = rgb(219/255, 210/255, 224/255,
-                                    alpha = alpha),
-                "nifu_orange" = rgb(232/255, 174/255, 89/255,
-                                    alpha = alpha))
+  if (reverse) pal <- rev(pal)
   
-  scales::manual_pal(unname(raw_cols))
+  colorRampPalette(pal, ...)
+  
 }
 
 
-#' NIFU-farger for omriss
+#' NIFU colour scale
 #'
-#' @inheritParams nifu_pal 
-#' @param ... Ekstra parametre for ggplot2::discrete_scale()
-#'
-#' @export scale_colour_nifu
-#' @rdname scale_nifu
+#' @param palette Palette name
+#' @param discrete Boolean indicating whether scale is discrete or not
+#' @param reverse Boolean indicating direction of palette
+#' @param ... Ekstra parametre for ggplot2::discrete_scale() or 
+#'            ggplot2::scale_colour_gradientn(), respectively
 #' 
 #' @importFrom ggplot2 discrete_scale
 #'
-scale_colour_nifu <- function(palette = c("default"), alpha = 1, ...){
-  palette <- match.arg(palette)
-  ggplot2::discrete_scale("colour", "nifu", nifu_pal(palette, alpha), ...)
+scale_colour_nifu <- function(palette = "main", 
+                              discrete = TRUE, 
+                              reverse = FALSE,
+                              ...){
+  
+  if(palette == "main" & !discrete){
+    warning("You have specified a non-discrete colour palette yet named a discrete palette. 
+            Consider providing one of c('reds', 'blues') to the 'palette' argument for better
+            results for continuous variables")
+  }
+  
+  pal <- nifu_pal(palette = palette, reverse = reverse)
+  
+  if(discrete){
+    ggplot2::discrete_scale(aesthetics = "colour", 
+                            scale_name = paste0("nifu_", palette), 
+                            palette = pal, 
+                            ...)
+  } else {
+    ggplot2::scale_colour_gradientn(colours = pal(256) ,...)
+  }
+  
 }
 
+#' @inheritParams scale_colour_nifu
 #' @export scale_color_nifu
 #' @rdname scale_nifu
 scale_color_nifu <- scale_colour_nifu
 
+
+
+#' NIFU fill scale
+#' 
+#' @inheritParams scale_colour_nifu
 #' @export scale_fill_nifu
 #' @importFrom ggplot2 discrete_scale
-#' @rdname scale_nifu
-scale_fill_nifu <- function(palette = c("default"), alpha = 1, ...) {
-  palette <- match.arg(palette)
-  ggplot2::discrete_scale("fill", "nifu", nifu_pal(palette, alpha), ...)
+#' 
+scale_fill_nifu <- function(palette = "main", 
+                              discrete = TRUE, 
+                              reverse = FALSE,
+                              ...){
+  
+  if(palette == "main" & !discrete){
+    warning("You have specified a non-discrete fill palette yet named a discrete palette. 
+            Consider providing one of c('reds', 'blues') to the 'palette' argument for better
+            results for continuous variables")
+  }
+  
+  pal <- nifu_pal(palette = palette, reverse = reverse)
+  
+  if(discrete){
+    ggplot2::discrete_scale(aesthetics = "fill", 
+                            scale_name = paste0("nifu_", palette), 
+                            palette = pal, 
+                            ...)
+  } else {
+    ggplot2::scale_fill_gradientn(colours = pal(256) ,...)
+  }
+  
 }
