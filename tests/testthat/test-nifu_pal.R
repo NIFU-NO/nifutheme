70f0e91edca00e06e318f7f56e7d856479174e00
Stephan Daus <stephus.daus@gmail.com>
2023-08-25 17:00:39 +0200

    Added NIFU template files and some tests.

--- tests/testthat/test-nifu_pal.R
+++ tests/testthat/test-nifu_pal.R
@@ -1,3 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+testthat::test_that("Correct classes", {
+  testthat::expect_true(inherits(nifutheme::scale_color_nifu(), 
+                                 what=class(ggplot2::scale_color_discrete())))
+  testthat::expect_true(inherits(nifutheme::scale_fill_nifu(), 
+                                 what=class(ggplot2::scale_color_discrete())))
+  testthat::expect_true(inherits(nifutheme::theme_nifu(), 
+                                 what=class(ggplot2::theme_classic())))
+  testthat::expect_equal(nifutheme::nifu_cols(), 
+                         c("#C84957",
+                           "#404040",
+                           "#EDE2D2",
+                           "#E8AE59",
+                           "#2D8E9F",
+                           "#DBD2E0"))
 })
